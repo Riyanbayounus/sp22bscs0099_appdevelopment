da47085a2cc2be05bf3c015f09e4af35089878a6
Faizan <k190348@nu.edu.pk>
2024-03-22 19:26:23 +0500

    Add project files.

---
+++ sp22bscs0099_appdevelopment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sp22bscs0099_appdevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
